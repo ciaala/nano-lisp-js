fec76c97025c1e01f229deebebebff97915beb52
Massimo Foti <massimo@massimocorner.com>
2016-02-08 18:29:54 +0100

    Unit tests

--- test/spec/jasmineTree.Spec.js
+++ test/spec/jasmineTree.Spec.js
@@ -17,9 +17,30 @@ describe("jasmineTree", function(){
 	});
 
 	describe(".init()", function(){
+
 		it("Is an utility method to bootstrap the GUI", function(){
 			expect(jasmineTree.Suite).toBeDefined();
 			expect($.isFunction(jasmineTree.Suite)).toBeTruthy();
+		});
+
+		describe("Invokes:", function(){
+
+			it(".addRootClass()", function(){
+				spyOn(jasmineTree, "addRootClass");
+				jasmineTree.init();
+				expect(jasmineTree.addRootClass).toHaveBeenCalled();
+			});
+			it(".addToolbar()", function(){
+				spyOn(jasmineTree, "addToolbar");
+				jasmineTree.init();
+				expect(jasmineTree.addToolbar).toHaveBeenCalled();
+			});
+			it(".filterSpec()", function(){
+				spyOn(jasmineTree, "filterSpec");
+				jasmineTree.init();
+				expect(jasmineTree.filterSpec).toHaveBeenCalled();
+			});
+
 		});
 	});
 
